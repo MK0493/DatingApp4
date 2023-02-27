01af9151aec9b83eed579de71e88e262f3a92d9b
Khan Muhammad Ashhad <mkhan@it-present.com>
2023-02-27 11:48:33 +0100

    End of Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,12 +1,11 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-
+        public byte[] PasswordHash { get; set; }
+        public byte[] passwordSalt { get; set; }
 
     }
 }
